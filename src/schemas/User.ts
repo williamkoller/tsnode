0647975b92eb0668df5dd8561952ca78698eae64
William Koller <wkoller25@gmail.com>
2020-03-11 21:56:35 -0300

    adding age on schema

--- src/schemas/User.ts
+++ src/schemas/User.ts
@@ -4,19 +4,21 @@ interface UserInterface extends Document {
   email?: string
   firstName?: string
   lastName?: string
+  age?: number
   fullName(): string
 }
 
 const UserSchema = new Schema({
   email: String,
   firstName: String,
-  lastName: String
+  lastName: String,
+  age: Number
 }, {
   timestamps: true
 })
 
 UserSchema.methods.fullName = function (): string {
-  return this.firstName + ' ' + this.lastName
+  return this.firstName + ' ' + this.lastName + this.age
 }
 
 export default model<UserInterface>('User', UserSchema)
